4ce78d21d7b261bf769ddfc489d0af042e19c9c1
kosoc <47340675+kosoc@users.noreply.github.com>
2019-02-04 17:27:18 -0800

    Update time1.sh

--- time1.sh
+++ time1.sh
@@ -3,17 +3,17 @@ echo "Start"
 while (true) 
 do
  ./75.sh;
- sleep 180; 
+ sleep 170; 
   ./50.sh;
- sleep 210; 
+ sleep 190; 
    ./80.sh;
- sleep 300; 
+ sleep 250; 
    ./65.sh;
- sleep 210; 
+ sleep 230; 
    ./90.sh;
- sleep 180; 
+ sleep 190; 
   ./40.sh;
- sleep 180; 
+ sleep 200; 
 done;
 
 
